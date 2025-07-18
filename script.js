2a2b6f4c66d5903747d9da70b117b12911d0d6f6
Riteshpal123 <115059467+Riteshpal123@users.noreply.github.com>
2025-07-18 11:43:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Countdown Timer to 31st July 2025 6PM
 const countdown = document.getElementById('countdown');
-const eventDate = new Date("August 3, 2025 18:00:00").getTime();
+const eventDate = new Date("July 31, 2025 18:00:00").getTime();
 
 setInterval(() => {
   const now = new Date().getTime();
